f5e2bc8114fa3730ade421dec90b1b3b84565027
ragge <>
2003-08-15 16:33:57 +0000

    Write data declarations via the passes, to make optimizing work.

--- arch/pdp10/local.c
+++ arch/pdp10/local.c
@@ -587,6 +587,8 @@ static CONSZ word;	/* word being built from fields */
 void
 incode(NODE *p, int sz)
 {
+	char *s;
+
 	inoff += sz;
 	if ((sz + inwd) > SZINT)
 		cerror("incode: field > int");
@@ -595,7 +597,9 @@ incode(NODE *p, int sz)
 
 	inwd += sz;
 	if (inoff % SZINT == 0) {
-		printf("	.long 0%llo\n", word);
+		s = isinlining ? permalloc(30) : tmpalloc(30);
+		sprintf(s, "	.long 0%llo", word);
+		send_passt(IP_ASM, s);
 		word = inwd = 0;
 	}
 	tfree(p);
@@ -656,10 +660,14 @@ cinit(NODE *p, int sz)
 void
 vfdzero(int n)
 {
+	char *s;
+
 	inoff += n;
 	inwd += n;
 	if (inoff%ALINT ==0) {
-		printf("	.long 0%llo\n", word);
+		s = isinlining ? permalloc(30) : tmpalloc(30);
+		sprintf(s, "	.long 0%llo", word);
+		send_passt(IP_ASM, s);
 		word = inwd = 0;
 	}
 }
