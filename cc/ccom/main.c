75d7aa24106fe1cd3a317713a3cad451dc332fdc
pj <>
2005-02-13 14:50:26 +0000

    Declare xnewreg

--- cc/ccom/main.c
+++ cc/ccom/main.c
@@ -44,7 +44,7 @@ int iTflag, oTflag;
 int xdebug, mdebug, sdebug, gflag;
 int Wstrict_prototypes, Wmissing_prototypes, Wimplicit_int,
 	Wimplicit_function_declaration;
-int xssaflag, xtailcallflag;
+int xssaflag, xtailcallflag, xnewreg;
 
 int e2debug, t2debug, f2debug;
 
