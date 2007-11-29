df1824cf15e9b93d2c33c46ba1a3062c80101a24
ragge <>
2007-11-29 16:03:23 +0000

    Let the target decide whether a init node is useable for init.

--- cc/ccom/init.c
+++ cc/ccom/init.c
@@ -490,9 +490,11 @@ scalinit(NODE *p)
 
 	p = optim(p);
 
+#ifdef notdef /* leave to the target to decide if useable */
 	if (csym->sclass != AUTO && p->n_op != ICON &&
 	    p->n_op != FCON && p->n_op != NAME)
 		cerror("scalinit not leaf");
+#endif
 
 	/* Out of elements? */
 	if (pstk == NULL) {
@@ -673,7 +675,7 @@ endinit(void)
 					infld(il->off, fsz, il->n->n_lval);
 				} else
 					ninval(il->off, fsz, il->n);
-				nfree(il->n);
+				tfree(il->n);
 			}
 			lastoff = ll->begsz + il->off + fsz;
 		}
