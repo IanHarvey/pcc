d17646d48248aac44b3be15b44fe76a7a93bd01c
ragge <>
2007-12-20 16:43:25 +0000

    Fix bug in qual handling so that ro data don't end up in rw segment.

--- cc/ccom/pftn.c
+++ cc/ccom/pftn.c
@@ -1779,7 +1779,7 @@ tymerge(NODE *typ, NODE *idp)
 #endif
 
 	idp->n_type = typ->n_type;
-	idp->n_qual = typ->n_qual;
+	idp->n_qual |= typ->n_qual;
 
 	tylkp = &tylnk;
 	tylkp->next = NULL;
@@ -1804,7 +1804,6 @@ tymerge(NODE *typ, NODE *idp)
 	/* now idp is a single node: fix up type */
 
 	idp->n_type = ctype(idp->n_type);
-//	idp->n_qual = DECQAL(idp->n_qual);
 
 	/* in case ctype has rewritten things */
 	if ((t = BTYPE(idp->n_type)) != STRTY && t != UNIONTY)
@@ -1921,8 +1920,10 @@ tyreduce(NODE *p, struct tylnk **tylkp, int *ntdim)
 	TWORD t, q;
 
 	o = p->n_op;
-	if (o == NAME)
+	if (o == NAME) {
+		p->n_qual = DECQAL(p->n_qual);
 		return;
+	}
 
 	t = INCREF(p->n_type);
 	q = p->n_qual;
