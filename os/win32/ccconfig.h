1bf3ca0ac515ff18c6c1608e53fd8ffdcf6326cf
gmcgarry <>
2008-05-18 00:49:55 +0000

    link against crt2.o when using mingw-runtime

--- os/win32/ccconfig.h
+++ os/win32/ccconfig.h
@@ -1,16 +1,16 @@
 #ifndef LIBDIR
-#define LIBDIR "/usr/lib"
+#define LIBDIR "/usr/lib/"
 #endif
 
-/* common cpp predefines */
-#define	CPPADD	{ "-DWIN32", NULL }
-
-/* host-dependent */
 #ifdef WIN32
+#define	CPPADD { "-DWIN32", NULL }
 #define LIBCLIBS { "/subsystem:console", "msvcrt.lib", NULL }
 #else
 /* requires w32api-3.2.tar.gz and mingw-runtime-3.14.tar.gz */
-#define LIBCLIBS { LIBDIR "libmsvcrt.a", NULL }
+#define CRT0FILE LIBDIR "crt2.o"
+#define CRT0FILE_PROFILE LIBDIR "gcrt2.o"
+#define	CPPADD { "-DWIN32", "-D__cdecl=", "-D__stdcall", NULL }
+#define LIBCLIBS { "-lmsvcrt", "-lmingw32", "-lkernel32", NULL }
 #endif
 
 #define CPPMDADD { "-D__i386__", NULL }
