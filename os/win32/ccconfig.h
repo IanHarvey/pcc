435160fcb4ff57d9826e518b22b0102c0f72cbbd
gmcgarry <>
2008-05-20 05:52:12 +0000

    Also link against libpcc

--- os/win32/ccconfig.h
+++ os/win32/ccconfig.h
@@ -14,7 +14,7 @@
 /* requires w32api-3.2.tar.gz and mingw-runtime-3.14.tar.gz */
 #define CRT0FILE LIBDIR "crt2.o"
 #define CRT0FILE_PROFILE LIBDIR "gcrt2.o"
-#define LIBCLIBS { "--enable-stdcall-fixup", "-lmsvcrt", "-lmingw32", "-lkernel32", "-luser32", NULL }
+#define LIBCLIBS { "--enable-stdcall-fixup", "-lmsvcrt", "-lmingw32", "-lkernel32", "-luser32", "-lpcc", NULL }
 #endif
 
 #define CPPMDADD { "-D__i386__", NULL }
