a886440b1304c3ea92c619f629988f3338475edf
gmcgarry <>
2008-06-21 23:34:11 +0000

    Add vsnprintf/snprintf compat routines for systems that require it.

--- common/compat.h
+++ common/compat.h
@@ -34,4 +34,12 @@ int mkstemp(char *);
 int ffs(int);
 #endif
 
+#ifndef HAVE_SNPRINTF
+int snprintf(char *str, size_t count, const char *fmt, ...);
+#endif
+
+#ifndef HAVE_VSNPRINTF
+int vsnprintf(char *str, size_t count, const char *fmt, va_list args);
+#endif
+
 #endif
