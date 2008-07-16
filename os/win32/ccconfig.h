5bc1ae7d0da88ac914a8543c97974ee53b1e8c56
gmcgarry <>
2008-07-16 10:18:42 +0000

    Mingw needs __MINGW32__ defined to build itself.

--- os/win32/ccconfig.h
+++ os/win32/ccconfig.h
@@ -6,12 +6,12 @@
  * Currently only supports console applications.
  */
 
-#define	CPPADD { "-DWIN32", NULL }
-
 #ifdef WIN32
+#define	CPPADD { "-DWIN32", NULL }
 #define LIBCLIBS { "/subsystem:console", "msvcrt.lib", NULL }
 #else
 /* requires w32api-3.2.tar.gz and mingw-runtime-3.14.tar.gz */
+#define	CPPADD { "-DWIN32", "-D__MINGW32__", NULL }
 #define CRT0FILE LIBDIR "crt2.o"
 #define CRT0FILE_PROFILE LIBDIR "gcrt2.o"
 #define LIBCLIBS { "--enable-stdcall-fixup", "-lmsvcrt", "-lmingw32", "-lkernel32", "-luser32", "-lpcc", NULL }
