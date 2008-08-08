a9332e417df1217e4e6a25db7223499d5d449afa
gmcgarry <>
2008-08-08 12:22:34 +0000

    Re-arrange startfiles for DLLs.

--- os/win32/ccconfig.h
+++ os/win32/ccconfig.h
@@ -12,9 +12,11 @@
 #else
 /* requires w32api-3.2.tar.gz and mingw-runtime-3.14.tar.gz */
 #define	CPPADD { "-DWIN32", "-D__MINGW32__", NULL }
-#define CRT0FILE LIBDIR "crt2.o"
-#define CRT0FILE_PROFILE LIBDIR "gcrt2.o"
-#define LIBCLIBS { "--enable-stdcall-fixup", "-lmsvcrt", "-lmingw32", "-lkernel32", "-luser32", "-lpcc", NULL }
+#define STARTFILES { LIBDIR "crt2.o", NULL }
+#define ENDFILES { NULL }
+#define STARTFILES_S { LIBDIR "dllcrt2.o", NULL }
+#define ENDFILES_S { NULL }
+#define LIBCLIBS { "-lmsvcrt", "-lmingw32", "-luser32", "-lkernel32", "-lpcc", "-lmsvcrt", NULL }
 #endif
 
 #define CPPMDADD { "-D__i386__", NULL }
