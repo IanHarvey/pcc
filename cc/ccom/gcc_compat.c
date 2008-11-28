d153f7197e804889a2d84204e77924209909d27a
ragge <>
2008-11-28 21:27:52 +0000

    Patch for __signed__ in Issue #PCC-6 from Detlef Riekenberg.

--- cc/ccom/gcc_compat.c
+++ cc/ccom/gcc_compat.c
@@ -58,6 +58,7 @@ static struct kw {
 /* 11 */{ "__typeof__", NULL, C_TYPEOF },
 /* 12 */{ "typeof", NULL, C_TYPEOF },
 /* 13 */{ "__extension__", NULL, -1 },
+/* 14 */{ "__signed__", NULL, 0 },
 	{ NULL, NULL, 0 },
 };
 
@@ -91,7 +92,8 @@ gcc_keyword(char *str, NODE **n)
 	if (kwp->rv)
 		return kwp->rv;
 	switch (i) {
-	case 1: /* __signed */
+	case 1:  /* __signed */
+	case 14: /* __signed__ */
 		*n = mkty((TWORD)SIGNED, 0, MKSUE(SIGNED));
 		return C_TYPE;
 	case 3: /* __const */
