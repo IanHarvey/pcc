31f5ba97cc80302c54d27f4e05983b2d24864dc8
ragge <>
2008-11-30 11:41:47 +0000

    Summarize number of edges in debug printout.

--- mip/regs.c
+++ mip/regs.c
@@ -1601,14 +1601,16 @@ livagain:
 		DLIST_FOREACH(y, &initial, link) {
 			printf("%d (%c): trivial [%d] ", ASGNUM(y),
 			    CLASS(y)+'@', trivially_colorable(y));
+			i = 0;
 			for (x = ADJLIST(y); x; x = x->r_next) {
 				if (ONLIST(x->a_temp) != &selectStack &&
 				    ONLIST(x->a_temp) != &coalescedNodes)
 					printf("%d ", ASGNUM(x->a_temp));
 				else
 					printf("(%d) ", ASGNUM(x->a_temp));
+				i++;
 			}
-			printf("\n");
+			printf(": n=%d\n", i);
 		}
 		printf("Move nodes\n");
 		DLIST_FOREACH(y, &initial, link) {
