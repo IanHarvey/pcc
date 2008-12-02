7815d3c9d50f0ca99bf036b7077b891ab3d217f9
gmcgarry <>
2008-12-02 21:37:15 +0000

    Add PCC_DEBUG and GCC_COMPAT to work around compile problems.  Link statically.

--- os/win32/build.bat
+++ os/win32/build.bat
@@ -12,9 +12,9 @@ set CCDIR=%BASEDIR%\cc\cc
 set OSDIR=%BASEDIR%\os\%TARGOS%
 set MACHDIR=%BASEDIR%\arch\%MACH%
 set BISON_SIMPLE=bison.simple
+set CFLAGS=/MT
 rem set CFLAGS=/Zi /MDd
-set CFLAGS=/MD
-set CPPFLAGS=-DWIN32 -DMSLINKER -DTARGOS=%TARGOS% -Dos_%TARGOS% -Dmach_%MACH% -DLIBEXECDIR=%LIBEXECDIR%
+set CPPFLAGS=-DWIN32 -DMSLINKER -DGCC_COMPAT -DPCC_DEBUG -DTARGOS=%TARGOS% -Dos_%TARGOS% -Dmach_%MACH% -DLIBEXECDIR=%LIBEXECDIR%
 
 cl /Fepcc.exe %CPPFLAGS% %CFLAGS% /I%CCDIR% /I. /I%MACHDIR% /I%MIPDIR% %CCDIR%\cc.c %MIPDIR%\compat.c
 
@@ -31,7 +31,7 @@ move y.tab.h cgram.h
 flex %CCOMDIR%\scan.l
 move lex.yy.c scan.c
 
-cl /Feccom.exe %CPPFLAGS% %CFLAGS% /I%CCOMDIR% /I%OSDIR% /I%MACHDIR% /I%MIPDIR% %CCOMDIR%\main.c %MIPDIR%\compat.c scan.c cgram.c external.c %CCOMDIR%\optim.c %CCOMDIR%\pftn.c %CCOMDIR%\trees.c %CCOMDIR%\inline.c %CCOMDIR%\symtabs.c %CCOMDIR%\gcc_compat.c %CCOMDIR%\init.c %MACHDIR%\local.c %MACHDIR%\code.c %CCOMDIR%\stabs.c %MIPDIR%\match.c %MIPDIR%\reader.c %MIPDIR%\optim2.c %MIPDIR%\regs.c %MACHDIR%\local2.c %MACHDIR%\order.c %MACHDIR%\table.c %MIPDIR%\common.c "C:\Program Files\UnxUtils\lib\libfl.lib"
+cl /Feccom.exe %CPPFLAGS% %CFLAGS% /I%CCOMDIR% /I%OSDIR% /I%MACHDIR% /I%MIPDIR% %CCOMDIR%\main.c %MIPDIR%\compat.c scan.c cgram.c external.c %CCOMDIR%\optim.c %CCOMDIR%\pftn.c %CCOMDIR%\trees.c %CCOMDIR%\inline.c %CCOMDIR%\symtabs.c %CCOMDIR%\gcc_compat.c %CCOMDIR%\init.c %MACHDIR%\local.c %MACHDIR%\code.c %CCOMDIR%\stabs.c %MIPDIR%\match.c %MIPDIR%\reader.c %MIPDIR%\optim2.c %MIPDIR%\regs.c %MACHDIR%\local2.c %MACHDIR%\order.c %MACHDIR%\table.c %MIPDIR%\common.c "C:\Program Files\UnxUtils\usr\local\lib\libfl.lib"
 
 md "C:\pcc"
 md "C:\pcc\bin"
