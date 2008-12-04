a7c65c784db386f9a97eb43e2bf92f5ebee4393f
ragge <>
2008-12-04 07:28:55 +0000

    Add SLIST macros for static init and isempty.

--- mip/manifest.h
+++ mip/manifest.h
@@ -225,8 +225,10 @@ void yyaccpt(void);
 /* Single-linked list */
 #define	SLIST_INIT(h)	\
 	{ (h)->q_forw = NULL; (h)->q_last = &(h)->q_forw; }
+#define	SLIST_SETUP(h) { NULL, &(h)->q_forw }
 #define	SLIST_ENTRY(t)	struct { struct t *q_forw; }
 #define	SLIST_HEAD(n,t) struct n { struct t *q_forw, **q_last; }
+#define	SLIST_ISEMPTY(h) ((h)->q_last == &(h)->q_forw)
 #define	SLIST_FIRST(h)	((h)->q_forw)
 #define	SLIST_FOREACH(v,h,f) \
 	for ((v) = (h)->q_forw; (v) != NULL; (v) = (v)->f.q_forw)
