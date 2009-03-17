5f4b88f8e94d8aaa257c692860b0fc66babf5d78
gmcgarry <>
2009-03-17 07:50:44 +0000

    On linux, use glibc-supplied crt1.o, crti.o and crtn.o.

--- os/linux/ccconfig.h
+++ os/linux/ccconfig.h
@@ -40,14 +40,14 @@
 
 #define	DYNLINKER { "-dynamic-linker", "/lib/ld-linux.so.2", NULL }
 
-#define CRT0FILE PCCLIBDIR "crt1.o"
-#define CRT0FILE_PROFILE PCCLIBDIR "gcrt1.o"
+#define CRT0FILE LIBDIR "crt1.o"
+#define CRT0FILE_PROFILE LIBDIR "gcrt1.o"
 
 #define	LIBCLIBS { "-lc", "-lpcc", NULL }
 #define	LIBCLIBS_PROFILE LIBCLIBS
 
-#define STARTFILES { PCCLIBDIR "crti.o", PCCLIBDIR "crtbegin.o", NULL }
-#define	ENDFILES { PCCLIBDIR "crtn.o", PCCLIBDIR "crtend.o", NULL }
+#define STARTFILES { LIBDIR "crti.o", PCCLIBDIR "crtbegin.o", NULL }
+#define	ENDFILES { LIBDIR "crtn.o", PCCLIBDIR "crtend.o", NULL }
 
 #define STARTLABEL "_start"
 
