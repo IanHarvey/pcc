4f2f423d6c0eb951d7e5433acf485f08580b2aca
gmcgarry <>
2009-03-18 07:48:30 +0000

    Define __MSVCRT__ so that types and structures match msvcrt interfaces.

--- os/win32/ccconfig.h
+++ os/win32/ccconfig.h
@@ -7,11 +7,12 @@
  */
 
 #if defined(WIN32) && defined(MSLINKER)
+/* requires microsoft toolkit headers and llnker */
 #define	CPPADD { "-DWIN32", NULL }
 #define LIBCLIBS { "/subsystem:console", "msvcrt.lib", "libpcc.a", NULL }
 #else
 /* requires w32api-3.2.tar.gz and mingw-runtime-3.14.tar.gz */
-#define	CPPADD { "-DWIN32", "-D__MINGW32__", NULL }
+#define	CPPADD { "-DWIN32", "-D__MSVCRT__", "-D__MINGW32__", NULL }
 #define STARTFILES { LIBDIR "crt2.o", NULL }
 #define ENDFILES { NULL }
 #define STARTFILES_S { LIBDIR "dllcrt2.o", NULL }
