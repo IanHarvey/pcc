f9f5a3da94efcdca85b42c042f8350c9e72f848f
ragge <>
2009-05-31 07:37:15 +0000

    Give amd64 flags on linux.

--- os/linux/ccconfig.h
+++ os/linux/ccconfig.h
@@ -55,6 +55,8 @@
 #define	CPPMDADD { "-D__i386__", NULL, }
 #elif defined(mach_powerpc)
 #define	CPPMDADD { "-D__ppc__", NULL, }
+#elif defined(mach_amd64)
+#define CPPMDADD { "-D__x86_64__", NULL, }
 #else
 #error defines for arch missing
 #endif
