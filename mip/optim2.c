328fd1b4a69ddf31a5a8464883f864cb1bb06e69
ragge <>
2009-06-06 19:19:55 +0000

    Change shadowing of variable name in prototypes.  -Wshadow complains in gcc3, but that is fixed in gcc4. Reported by Jonathan Gray.

--- mip/optim2.c
+++ mip/optim2.c
@@ -86,12 +86,12 @@ static void do_cse(struct p2env* p2e) ;
 
 /* Walk the complete set, performing a function on each node. 
  * if type is given, apply function on only that type */
-void WalkAll(struct p2env* p2env, void (*f) (NODE*, void*), void* arg, int type) ;
+void WalkAll(struct p2env* p2e, void (*f) (NODE*, void*), void* arg, int type) ;
 
 void BBLiveDead(struct basicblock* bblock, int what, unsigned int variable) ;
 
 /* Fill the live/dead code */
-void LiveDead(struct p2env* p2env, int what, unsigned int variable) ;
+void LiveDead(struct p2env* p2e, int what, unsigned int variable) ;
 
 #ifdef PCC_DEBUG
 void printflowdiagram(struct p2env *,struct labelinfo *labinfo,struct bblockinfo *bbinfo,char *);
