e44f7df2461853334a4580f3984113d5d3b7d5e7
gmcgarry <>
2009-09-17 02:58:22 +0000

    Improve building of shared libraries on linux.  Tested on Ubuntu 8.04.

--- os/linux/ccconfig.h
+++ os/linux/ccconfig.h
@@ -36,25 +36,28 @@
 #endif
 
 /* common cpp predefines */
-#define	CPPADD	{ "-D__linux__", "-D__ELF__", NULL, }
+#define CPPADD	{ "-D__linux__", "-D__ELF__", NULL, }
 
 #define CRT0FILE LIBDIR "crt1.o"
 #define CRT0FILE_PROFILE LIBDIR "gcrt1.o"
 
-#define	LIBCLIBS { "-lc", "-lpcc", NULL }
-#define	LIBCLIBS_PROFILE LIBCLIBS
+#define LIBCLIBS { "-lc", "-lpcc", NULL }
+#define LIBCLIBS_PROFILE LIBCLIBS
 
 #define STARTFILES { LIBDIR "crti.o", PCCLIBDIR "crtbegin.o", NULL }
-#define	ENDFILES { PCCLIBDIR "crtend.o", LIBDIR "crtn.o", NULL }
+#define ENDFILES { PCCLIBDIR "crtend.o", LIBDIR "crtn.o", NULL }
+
+#define STARTFILES_S { LIBDIR "crti.o", PCCLIBDIR "crtbegin.o", NULL }
+#define ENDFILES_S { PCCLIBDIR "crtend.o", LIBDIR "crtn.o", NULL }
 
 #define STARTLABEL "_start"
 
 #if defined(mach_i386)
-#define	CPPMDADD { "-D__i386__", NULL, }
-#define	DYNLINKER { "-dynamic-linker", "/lib/ld-linux.so.2", NULL }
+#define CPPMDADD { "-D__i386__", NULL, }
+#define DYNLINKER { "-dynamic-linker", "/lib/ld-linux.so.2", NULL }
 #elif defined(mach_powerpc)
-#define	CPPMDADD { "-D__ppc__", NULL, }
-#define	DYNLINKER { "-dynamic-linker", "/lib/ld-linux.so.2", NULL }
+#define CPPMDADD { "-D__ppc__", NULL, }
+#define DYNLINKER { "-dynamic-linker", "/lib/ld-linux.so.2", NULL }
 #elif defined(mach_amd64)
 #define CPPMDADD { "-D__x86_64__", NULL, }
 #define	DYNLINKER { "-dynamic-linker", "/lib64/ld-linux-x86-64.so.2", NULL }
@@ -62,5 +65,5 @@
 #error defines for arch missing
 #endif
 
-#define	STABS
+#define STABS
 #define ELFABI
