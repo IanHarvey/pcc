f4a2b7242b9ca269edac23706aba5afba04fa57b
ragge <>
2010-09-19 13:54:41 +0000

    Fix to make mips compile again, from Alt.

--- arch/mips/table.c
+++ arch/mips/table.c
@@ -1004,7 +1004,7 @@ struct optab table[] = {
 { STASG,        INAREG|FOREFF,
         SOREG|SNAME,	TANY,
         SAREG,  	TPTRTO|TANY,
-                NSPECIAL,       RRIGHT,
+                NSPECIAL,       RDEST,
                 "ZQ", },
 
 /*
