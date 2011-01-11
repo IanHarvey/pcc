8ee9ad13695634a613a63c50960dc56fc59688b6
ragge <>
2011-01-11 11:01:01 +0000

    Fix a "arguments differ in signedness" bug, reported by Iain Hibbert.

--- cc/cpp/cpp.c
+++ cc/cpp/cpp.c
@@ -218,7 +218,7 @@ main(int argc, char **argv)
 	/* create a complete macro for pragma */
 	pragloc = lookup((const usch *)"_Pragma", ENTER);
 	savch(0);
-	savstr("_Pragma(");
+	savstr((const usch *)"_Pragma(");
 	savch(0);
 	savch(WARN);
 	savch(')');
