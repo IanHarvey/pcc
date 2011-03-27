73b6c7ad1717b7280d27581cd1421b6638603e6a
ragge <>
2011-03-27 19:16:06 +0000

    MKSUE -> MKAP for win32 also.  Fixes Jira#PCC-276 by Richard Spencer.

--- arch/i386/local.c
+++ arch/i386/local.c
@@ -118,7 +118,7 @@ import(NODE *p)
 
 	sp = picsymtab("__imp_", name, "");
 	q = xbcon(0, sp, PTR+VOID);
-	q = block(UMUL, q, 0, PTR|VOID, 0, MKSUE(VOID));
+	q = block(UMUL, q, 0, PTR|VOID, 0, MKAP(VOID));
 	q = block(UMUL, q, 0, p->n_type, p->n_df, p->n_sue);
 	q->n_sp = p->n_sp; /* for init */
 	nfree(p);
