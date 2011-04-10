52ecc2297b293010bfddfd49ca76587c72c50bee
ragge <>
2011-04-10 15:24:53 +0000

    Do not try to inline variadic functions.  Comment said it whouldn't, but the code was missing.  Now it's added.  Fixes Jira#PCC-294 by David Soria Parra.

--- cc/ccom/inline.c
+++ cc/ccom/inline.c
@@ -511,7 +511,9 @@ inlinetree(struct symtab *sp, NODE *f, NODE *ap)
 void
 inline_args(struct symtab **sp, int nargs)
 {
+	union arglist *al;
 	struct istat *cf;
+	TWORD t;
 	int i;
 
 	SDEBUG(("inline_args\n"));
@@ -521,6 +523,20 @@ inline_args(struct symtab **sp, int nargs)
 	 * - function has varargs
 	 * - function args are volatile, checked if no temp node is asg'd.
 	 */
+	/* XXX - this is ugly, invent something better */
+	if (cf->sp->sdf->dfun == NULL)
+		return; /* no prototype */
+	for (al = cf->sp->sdf->dfun; al->type != TNULL; al++) {
+		t = al->type;
+		if (t == TELLIPSIS)
+			return; /* cannot inline */
+		if (ISSOU(BTYPE(t)))
+			al++;
+		for (; t > BTMASK; t = DECREF(t))
+			if (ISARY(t) || ISFTN(t))
+				al++;
+	}
+
 	if (nargs) {
 		for (i = 0; i < nargs; i++)
 			if ((sp[i]->sflags & STNODE) == 0)
