f88246a394e927955d611c38b41e3c7d355fd8b6
ragge <>
2011-04-26 15:16:50 +0000

    Must set struct alignment to a sane size too. Reported by iain Hibbert in private mail.

--- cc/ccom/gcc_compat.c
+++ cc/ccom/gcc_compat.c
@@ -469,12 +469,13 @@ gcc_tcattrfix(NODE *p)
 {
 	struct symtab *sp;
 	struct attr *ap;
-	int sz, coff, csz, al, oal;
+	int sz, coff, csz, al, oal, mxal;
 
 	if ((ap = attr_find(p->n_ap, GCC_ATYP_PACKED)) == NULL)
 		return; /* nothing to fix */
 
 	al = ap->iarg(0);
+	mxal = 0;
 
 	/* Must repack struct */
 	coff = csz = 0;
@@ -482,6 +483,8 @@ gcc_tcattrfix(NODE *p)
 		oal = talign(sp->stype, sp->sap);
 		if (oal > al)
 			oal = al;
+		if (mxal < oal)
+			mxal = oal;
 		if (sp->sclass & FIELD)
 			sz = sp->sclass&FLDSIZ;
 		else
@@ -492,12 +495,13 @@ gcc_tcattrfix(NODE *p)
 		if (p->n_type == UNIONTY)
 			coff = 0;
 	}
-	SETOFF(csz, al); /* Roundup to whatever */
+	SETOFF(csz, mxal); /* Roundup to whatever */
 
 	ap = attr_find(p->n_ap, ATTR_STRUCT);
 	ap->amsize = csz;
 	ap = attr_find(p->n_ap, GCC_ATYP_ALIGNED);
-	ap->iarg(0) = al;
+	ap->iarg(0) = mxal;
+
 }
 
 /*
