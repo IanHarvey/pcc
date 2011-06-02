fc8281aaf39e4875c958402dcef113c8232f66a2
ragge <>
2011-06-02 15:31:55 +0000

    Word-addressed targets.  While here, remove clause 3.

--- arch/nova/macdefs.h
+++ arch/nova/macdefs.h
@@ -11,8 +11,6 @@
  * 2. Redistributions in binary form must reproduce the above copyright
  *    notice, this list of conditions and the following disclaimer in the
  *    documentation and/or other materials provided with the distribution.
- * 3. The name of the author may not be used to endorse or promote products
- *    derived from this software without specific prior written permission
  *
  * THIS SOFTWARE IS PROVIDED BY THE AUTHOR ``AS IS'' AND ANY EXPRESS OR
  * IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES
@@ -87,6 +85,7 @@
 
 /* Default char is unsigned */
 #define	CHAR_UNSIGNED
+#define	WORD_ADDRESSED
 
 /*
  * Use large-enough types.
