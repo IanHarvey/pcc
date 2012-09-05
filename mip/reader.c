4368e9e08a8436534d74e639da4f49c4c30b1f68
ragge <>
2012-09-05 16:05:12 +0000

    Do deljumps() even id not ssa is given.

--- mip/reader.c
+++ mip/reader.c
@@ -409,7 +409,7 @@ pass2_compile(struct interpass *ip)
 	optimize(p2e);
 	ngenregs(p2e);
 
-	if (xssa && xtemps && xdeljumps)
+	if (xtemps && xdeljumps)
 		deljumps(p2e);
 
 	DLIST_FOREACH(ip, &p2e->ipole, qelem)
