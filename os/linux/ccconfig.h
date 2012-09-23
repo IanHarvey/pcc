cf242511ff203b3f086068a81be8075cc2e7d427
ragge <>
2012-09-23 09:45:04 +0000

    Fix typo; CRT should be CRT0, as pointed out by Thorsten Glaser.

--- os/linux/ccconfig.h
+++ os/linux/ccconfig.h
@@ -32,7 +32,7 @@
 /* common cpp predefines */
 #define CPPADD	{ "-D__linux__", "-D__ELF__", NULL, }
 
-#define CRT		"crt1.o"
+#define CRT0		"crt1.o"
 #define GCRT0		"gcrt1.o"
 
 #define STARTLABEL "_start"
