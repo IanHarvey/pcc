0cfb11c403f37325be9b769abf7b6212413fa1d2
ragge <>
2015-11-06 16:41:24 +0000

    Fix syntax for including "xxx" files. Reported by Iain Hibbert on pcc-list.

--- cc/cpp/cpp.c
+++ cc/cpp/cpp.c
@@ -686,7 +686,7 @@ void
 include(void)
 {
 	struct iobuf *ob;
-	usch *fn, *nm;
+	usch *fn, *nm = NULL;
 
 	if (flslvl)
 		return;
@@ -700,9 +700,6 @@ include(void)
 	if (fn[0] == '/' && pushfile(fn, fn, 0, NULL) == 0)
 		goto okret;
 	if (fn[-1] == '\"') {
-		/* local includes. first try directly. */
-		if (pushfile(fn, fn, 0, NULL) == 0)
-			goto okret;
 		/* nope, failed, try to create a path for it */
 		if ((nm = (usch *)strrchr((char *)ifiles->orgfn, '/'))) {
 			ob = strtobuf((usch *)ifiles->orgfn, NULL);
@@ -711,10 +708,12 @@ include(void)
 			putob(ob, 0);
 			nm = xstrdup(ob->buf);
 			bufree(ob);
-			if (pushfile(nm, nm, 0, NULL) == 0) {
-				free(fn-1);
-				goto okret;
-			}
+		} else {
+			nm = xstrdup(fn);
+		}
+		if (pushfile(nm, nm, 0, NULL) == 0) {
+			free(fn-1);
+			goto okret;
 		}
 	}
 	if (fsrch(fn, 0, incdir[0]))
@@ -724,6 +723,8 @@ include(void)
 	/* error() do not return */
 
 okret:
+	if (nm)
+		free(nm);
 	prtline(1);
 }
 
