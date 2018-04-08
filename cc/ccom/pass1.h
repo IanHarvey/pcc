e5ef7793bcfd0571cbbb2170cec2161c552b5d4f
ragge <>
2018-04-08 07:35:41 +0000

    No local flags in symtab, use attributes instead.

--- cc/ccom/pass1.h
+++ cc/ccom/pass1.h
@@ -97,14 +97,11 @@ extern	char *scnames(int);
 #define STEMP		00100	/* Allocate symtab from temp or perm mem */
 #define	SDYNARRAY	00200	/* symbol is dynamic array on stack */
 #define	SINLINE		00400	/* function is of type inline */
-#define	SINREG		SINLINE	/* variable is put in reg */
 #define	SBLK		SINLINE	/* Allocate symtab from blk mem */
 #define	STNODE		01000	/* symbol shall be a temporary node */
 #define	SBUILTIN	02000	/* this is a builtin function */
 #define	SASG		04000	/* symbol is assigned to already */
-#define	SLOCAL1		010000
-#define	SLOCAL2		020000
-#define	SLOCAL3		040000
+#define	SINREG		010000	/* variable is put in reg */
 
 	/* alignment of initialized quantities */
 #ifndef AL_INIT
